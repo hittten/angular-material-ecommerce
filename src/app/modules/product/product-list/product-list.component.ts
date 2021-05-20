86fb49d3a4c0954e0fb3b8dd6a3a58fe42aceab0
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-05-20 15:51:27 +0200

    grid view and pipes

--- src/app/modules/product/product-list/product-list.component.ts
+++ src/app/modules/product/product-list/product-list.component.ts
@@ -4,14 +4,27 @@ import {ThemePalette} from "@angular/material/core";
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {NgFor, NgOptimizedImage, NgIf} from '@angular/common';
+import {NgFor, NgOptimizedImage, NgIf, UpperCasePipe, DatePipe} from '@angular/common';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
   standalone: true,
-  imports: [NgFor, NgOptimizedImage, MatButtonModule, MatIconModule, NgIf, MatDividerModule]
+  imports: [
+    NgFor,
+    NgOptimizedImage,
+    MatButtonModule,
+    MatIconModule,
+    NgIf,
+    MatDividerModule,
+    MatButtonToggleModule,
+    UpperCasePipe,
+    DatePipe,
+    MatCardModule
+  ]
 })
 export class ProductListComponent {
   @Input() products: Product[] = [];
