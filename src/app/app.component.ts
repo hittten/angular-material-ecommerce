581ebe3b6ef547b56412e391af266a91a6277f9b
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2023-11-07 23:28:03 +0100

    product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PRODUCTS} from "./mock-products";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ecommerce';
+  products = PRODUCTS;
 }
---
+++ src/app/mock-products.ts
@@ -0,0 +1,182 @@
+import {Product} from './product';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: '1',
+    name: 'product #1',
+    price: 11,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/10/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '2',
+    name: 'product #2',
+    price: 12,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo',
+    image: 'https://picsum.photos/id/20/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '3',
+    name: 'product #3',
+    price: 13,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/30/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '4',
+    name: 'product #4',
+    price: 14,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo',
+    image: 'https://picsum.photos/id/40/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '5',
+    name: 'product #5',
+    price: 15,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/50/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '6',
+    name: 'product #6',
+    price: 16,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/60/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '7',
+    name: 'product #7',
+    price: 17,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/70/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '8',
+    name: 'product #8',
+    price: 18,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/80/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '9',
+    name: 'product #9',
+    price: 19,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/90/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '10',
+    name: 'product #10',
+    price: 110,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/100/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '11',
+    name: 'product #11',
+    price: 111,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/15/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '12',
+    name: 'product #12',
+    price: 112,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/25/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '13',
+    name: 'product #13',
+    price: 113,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/35/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '14',
+    name: 'product #14',
+    price: 114,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/45/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '15',
+    name: 'product #15',
+    price: 115,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/55/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '16',
+    name: 'product #16',
+    price: 116,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/65/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '17',
+    name: 'product #17',
+    price: 117,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/75/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '18',
+    name: 'product #18',
+    price: 118,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/85/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '19',
+    name: 'product #19',
+    price: 119,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/95/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+  {
+    id: '20',
+    name: 'product #20',
+    price: 120,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, fugiat nam? Animi commodi, debitis est, explicabo' +
+      'magni nisi rem repellendus reprehenderit repudiandae tenetur ullam velit? Aspernatur consequuntur sunt vitae voluptatibus!',
+    image: 'https://picsum.photos/id/115/300/300',
+    createdAt: '2019-12-31T23:00:00.000Z',
+  },
+];
