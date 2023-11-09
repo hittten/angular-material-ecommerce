2ade7e67138283a23652fc5f106ea1d34d62d1ca
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2023-11-09 23:30:00 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AngularEcommerce
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.4.
 
 ## Development server
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import {PreloadAllModules, provideRouter, withPreloading} from '@angular/router'
 
 import {routes} from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimations(),
+    provideAnimationsAsync(),
   ]
 };
