f6a3d675c6e962294821f3dcf2dc8d1bbea88256
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2023-11-22 15:18:02 +0100

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
+import {SwUpdate} from "@angular/service-worker";
+import {filter} from "rxjs";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +14,23 @@ import {HeaderComponent} from './components/header/header.component';
   imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent {
+  private snackBar = inject(MatSnackBar)
+  private updates = inject(SwUpdate)
+
+  constructor() {
+    this.updates.versionUpdates.pipe(
+      takeUntilDestroyed(),
+      filter(event => event.type === 'VERSION_READY'),
+    )
+      .subscribe((versionEvent) => {
+        console.log('build version', versionEvent)
+        this.snackBar
+          .open('Hay una versión nueva de la aplicación', 'Actualizar')
+          .onAction().pipe(takeUntilDestroyed())
+          .subscribe(async () => {
+            await this.updates.activateUpdate()
+            document.location.reload()
+          })
+      })
+  }
 }
